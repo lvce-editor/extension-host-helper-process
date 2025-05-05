e84ea200478f82a5755fb00bbc3bc754ede7cf08
Le Vivilet <levivlet@pm.me>
2025-05-05 20:27:32 +0200

    feature: reduce max memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const instantiations = 2_000
+export const instantiations = 1200
 
 export const instantiationsPath = join(root, 'packages', 'extension-host-helper-process')
